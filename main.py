baeab202d46ee317bbecadb1bd4e32b34375df69
YuriyKulyck <141341450+bpopivnyak@users.noreply.github.com>
2024-02-04 18:29:30 +0200

    vi vse pidarasy

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+valcode = "USD"
+date = "20230204"
+a = requests.get(f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={valcode}&date={date}&json")
+
+if a.status_code == 200:
+    data = json.loads(a.text)
+    print(data[0]["txt"], data[0]["rate"])
